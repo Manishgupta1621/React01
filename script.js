5188baf0497e648f0994fe707d4cf8a1d71710e6
Manishgupta1621 <manishgupta1621307@gmail.com>
2024-12-20 11:42:45 +0530

    part-01

--- script.js
+++ script.js
@@ -6,19 +6,18 @@
 
 const parent = React.createElement(
     "div",
-    {id:"parent"},
-    [ React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement("h1", {}, "I am h1 tags"), React.createElement("h2", {}, "I am h2 tags")]
-    ),
+    { id: "parent" },
+    [React.createElement(
+        "div", { id: "child" }, [
+        React.createElement("h1", {}, "I am h1 tags"),
+        React.createElement("h2", {}, "I am h2 tags"),
+    ]),
     React.createElement(
-        "div",
-        {id:"child2"},
-        [React.createElement("h1", {}, "I am h1 tags"), React.createElement("h2", {}, "I am h2 tags")]
-    )]
-
-);
+        "div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am h1 tags"),
+        React.createElement("h2", {}, "I am h2 tags"),
+    ]),
+    ]);
 
 
 
